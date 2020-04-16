43538de3ab5e03170b26c24b9e09bdbcec243458
gerarjon <gerarsuaverdez@gmail.com>
2020-04-15 19:19:16 -0700

    added dotenv and proxy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -21,6 +22,15 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:students")
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+
+//Get the default connection
+const db = mongoose.connection;
+
+db.once('open', () => console.log('connected to the database'));
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
